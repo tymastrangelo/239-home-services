6db18aa79ba5a2a444813c61149710adaa78448d
tyler <mastrangelo.tyler@gmail.com>
2025-10-03 22:30:35 -0400

    github pages fix

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/239-home-services/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
